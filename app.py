748882e8610d62e9a7afa78ed0c208a2a975fea9
Condesay <39847380+condesay@users.noreply.github.com>
2023-03-25 17:53:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 
+# Définition d'une liste de personnes
+personnes = [
+    {"nom": "Jane Doe", "age": 28, "niveau_anxiete": "Assez", "description": "Je suis une personne calme et réfléchie, j'aime aider les autres à surmonter leurs peurs."},
+    {"nom": "John Smith", "age": 35, "niveau_anxiete": "Très", "description": "Je suis un passionné de sports extrêmes et j'aime défier mes limites."},
+    {"nom": "Alice Johnson", "age": 42, "niveau_anxiete": "Un peu", "description": "Je suis une personne optimiste qui aime découvrir de nouvelles choses."},
+    {"nom": "Bob Brown", "age": 50, "niveau_anxiete": "Pas du tout", "description": "Je suis une personne très détendue qui ne se laisse pas facilement stresser."},
+    {"nom": "Sarah Lee", "age": 25, "niveau_anxiete": "Assez", "description": "Je suis une personne créative qui trouve souvent des solutions originales aux problèmes."},
+]
+
 # Titre de l'application
 st.title("Mon application de jeu")
 st.image("image.png", caption="Une image pour vous inspirer")
@@ -31,16 +40,8 @@ st.write("Si vous avez besoin d'aide pour relever votre défi, vous pouvez vous 
 nom_recherche = st.text_input("Entrez le nom de la personne que vous cherchez")
 niveau_anxiete_recherche = st.selectbox("À quel niveau d'anxiété cette personne doit-elle être ?", ["Pas du tout", "Un peu", "Assez", "Très"])
 
-# Bouton pour rechercher une personne
-if st.button("Rechercher une personne"):
+# Bouton pour rechercher des personnes
+if st.button("Rechercher des personnes"):
     st.write("Voici les résultats de votre recherche :")
-    # Requête à une base de données (hypothétique) pour trouver une personne correspondant aux critères de recherche
-    # Affichage des résultats de la recherche
-    st.write("Nom : Jane Doe")
-    st.write("Âge : 28 ans")
-    st.write("Niveau d'anxiété : Assez")
-    st.write("Description : Je suis une personne calme et réfléchie, j'aime aider les autres à surmonter leurs peurs.")
-
-    # Bouton pour contacter la personne
-    if st.button("Contacter cette personne"):
-        st.write("Vous avez contacté Jane Doe !")
+    # Requête à une base de données (hypothétique) pour trouver les personnes correspondant aux critères de recherche
+    # Pour cet exemple, nous filtrons simplement les personnes de la liste
