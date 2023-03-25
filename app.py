1e422b0d8080e8e9d3acc19a5654bca7127dc485
Condesay <39847380+condesay@users.noreply.github.com>
2023-03-25 17:29:09 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+# Titre de l'application
+st.title("Mon application de jeu")
+
+# Formulaire pour obtenir les informations de l'utilisateur
+nom_utilisateur = st.text_input("Entrez votre nom")
+difficulte = st.selectbox("Choisissez votre niveau de difficulté", ["Facile", "Moyen", "Difficile"])
+
+# Affichage des informations de l'utilisateur
+st.write("Bienvenue", nom_utilisateur, "!")
+st.write("Vous avez choisi le niveau de difficulté", difficulte)
+
+# Définition des défis en fonction du niveau de difficulté
+if difficulte == "Facile":
+    st.write("Défi facile : dessinez une image de vous-même")
+elif difficulte == "Moyen":
+    st.write("Défi moyen : écrivez un paragraphe sur votre plus grande peur")
+else:
+    st.write("Défi difficile : contactez quelqu'un que vous n'avez pas parlé depuis longtemps")
+
+# Affichage d'une image
+st.image("image.png", caption="Une image pour vous inspirer")
+
+# Bouton pour commencer le jeu
+if st.button("Commencer le jeu"):
+    st.write("Le jeu commence !")
